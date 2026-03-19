841fdd8d2664f4ea51bf57b8c9505726ef017645
Rakshitha R <rakshitha@gmail.com>
2026-03-19 04:33:42 -0400

    Fixed Main.java content

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("AI Parking System Started...");
+        System.out.println("Parking slots detected successfully!");
+    }
+}
